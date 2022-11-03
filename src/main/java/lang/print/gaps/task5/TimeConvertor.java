4cef613334c0a26122935dd9042da77d9f18e8dd
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-03 12:28:54 +0500

    "fir"

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
+import java.util.Date;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+        float sec = minutes/3600;
+        System.out.println(sec);
     }
+
+
 }
